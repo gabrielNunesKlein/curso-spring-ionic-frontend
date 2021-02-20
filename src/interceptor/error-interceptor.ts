c20d1d4c6617da556cfbc669f2a5a14b8a57ecc9
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-02-20 10:59:02 -0300

    Tratando erros 403

--- src/interceptor/error-interceptor.ts
+++ src/interceptor/error-interceptor.ts
@@ -2,10 +2,14 @@ import { Injectable } from "@angular/core";
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Observable} from "rxjs";
 import { catchError } from 'rxjs/operators';
+import { StorageService } from "../services/storage.service";
  
  
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor{
+
+    constructor(public storage: StorageService){
+    }
   
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
      
@@ -23,9 +27,19 @@ export class ErrorInterceptor implements HttpInterceptor{
                         //alert("erro inteceptor");
                         console.log("errorObj");
                         console.log(errorObj);
+
+                        switch (errorObj.status) {
+                            case 403:
+                                this.handle403()
+                                break;
+                        }
                        
                         return Observable.throw(errorObj);
                     })) as any;
+    }
+
+    handle403(){
+        this.storage.setLocalUser(null)
     }
   
 } 
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -25,7 +25,14 @@ export class ProfilePage {
         this.cliente = Response
         this.getImageIfExist()
       },
-      error => {})
+      error => {
+        if(error.status == 403){
+          this.navCtrl.setRoot('HomePage')
+        }
+      })
+    }
+    else{
+      this.navCtrl.setRoot('HomePage')
     }
   }
 
