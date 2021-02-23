e6ddd0f1cea9ff1ebcd61cb9d39c4941939a55a2
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-02-23 10:46:43 -0300

    Carregando imagens dos produtos

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
 import { ProdutoDTO } from '../../models/Produto.dto';
 import { ProdutoService } from '../../services/domain/produto.services';
 
@@ -19,6 +20,16 @@ export class ProdutosPage {
     let categoria_id = this.navParams.get('categoria_id')
     this.produtoService.findByCategoria(categoria_id).subscribe(Response => {
       this.items = Response['content']
+      this.loadImageUrls()
     }, error => {})
+  }
+
+  loadImageUrls(){
+    for(var i=0; i<this.items.length; i++){
+      let item = this.items[i]
+      this.produtoService.getSmallImageFromBucket(item.id).subscribe(response => {
+        item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`
+      }, error => {})
+    }
   }
 }
