d234c8f4f9d2504a58c4ade997a70a8c296f8cf6
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-03-01 12:54:11 -0300

    Informando pedido registrado ao usuário

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -20,6 +20,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codpedido: string;
   
 
   constructor(public navCtrl: NavController, public navParams: NavParams, 
@@ -53,14 +54,23 @@ export class OrderConfirmationPage {
     this.navCtrl.setRoot('CartPage')
   }
 
+  home(){
+    this.navCtrl.setRoot('CategoriasPage')
+  }
+
   checkout(){
     this.pedidoService.insert(this.pedido).subscribe(Response => {
       this.cartService.createOrClearCart()
-      console.log(Response.headers.get('location'))
+      this.codpedido = this.extractId(Response.headers.get('location'))
     }, error =>{
       if(error.status == 403){
         this.navCtrl.setRoot('HomePage')
       }
     })
+  }
+
+  private extractId(location: string): string{
+    let position = location.lastIndexOf('/')
+    return location.substring(position + 1, location.length)
   }
 }
